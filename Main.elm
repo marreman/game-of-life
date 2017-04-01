4f90dcee6e613e9f70f198993099fcd156be7c2c
Martin Liberg <martin.liberg@izettle.com>
2017-04-01 23:04:56 +0200

    Game is running

--- Main.elm
+++ Main.elm
@@ -1,22 +1,26 @@
 module Main exposing (..)
 
-import Html
+import Html exposing (Html)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Svg.Events exposing (..)
 import Matrix exposing (Matrix, Location)
+import Time exposing (Time)
 
 
 main =
-    Html.beginnerProgram
-        { model = model
+    Html.program
+        { init = ( model, Cmd.none )
+        , subscriptions = subscriptions
         , update = update
         , view = view
         }
 
 
 type alias Model =
-    { grid : Matrix Bool }
+    { started : Bool
+    , grid : Matrix Bool
+    }
 
 
 type alias Position =
@@ -25,30 +29,108 @@ type alias Position =
 
 model : Model
 model =
-    { grid = Matrix.matrix 10 10 (\_ -> False) }
+    { started = False
+    , grid = Matrix.square 100 (\_ -> False)
+    }
 
 
 type Msg
-    = Flip Location
+    = StartStop
+    | Flip Location
+    | Tick Time
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Flip location ->
-            { model | grid = Matrix.update location not model.grid }
+            { model | grid = Matrix.update location not model.grid } ! []
+
+        StartStop ->
+            { model | started = not model.started } ! []
+
+        Tick _ ->
+            { model | grid = evolveGrid model.grid } ! []
+
+
+evolveGrid : Matrix Bool -> Matrix Bool
+evolveGrid grid =
+    let
+        evolve : Location -> Bool -> Bool
+        evolve location _ =
+            getNeighbours location
+                |> List.map (\loc -> Matrix.get loc grid)
+                |> List.map evaluateCell
+                |> List.sum
+                |> evaluateResult
+
+        getNeighbours : Location -> List Location
+        getNeighbours ( x, y ) =
+            [ Matrix.loc (x + 1) (y + 0)
+            , Matrix.loc (x + 1) (y + 1)
+            , Matrix.loc (x + 0) (y + 1)
+            , Matrix.loc (x - 1) (y + 1)
+            , Matrix.loc (x - 1) (y + 0)
+            , Matrix.loc (x - 1) (y - 1)
+            ]
+
+        evaluateCell : Maybe Bool -> Int
+        evaluateCell maybeAlive =
+            maybeAlive
+                |> Maybe.map
+                    (\isAlive ->
+                        if isAlive then
+                            1
+                        else
+                            0
+                    )
+                |> Maybe.withDefault 0
+
+        evaluateResult : Int -> Bool
+        evaluateResult numberOfNeighbours =
+            case numberOfNeighbours of
+                3 ->
+                    True
+
+                _ ->
+                    False
+    in
+        Matrix.mapWithLocation evolve grid
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    if model.started then
+        Time.every Time.second Tick
+    else
+        Sub.none
 
 
 view : Model -> Svg Msg
 view model =
+    Html.div []
+        [ viewControls
+        , viewGrid model.grid
+        ]
+
+
+viewControls : Html Msg
+viewControls =
+    Html.div []
+        [ Html.button [ onClick StartStop ] [ Html.text "play" ] ]
+
+
+viewGrid : Matrix Bool -> Svg Msg
+viewGrid grid =
     let
         squares =
-            model.grid
+            grid
                 |> Matrix.mapWithLocation square
                 |> Matrix.flatten
     in
         svg
-            [ width "600"
+            [ y "100"
+            , width "600"
             , height "600"
             , viewBox ("0 0 10 10")
             ]
@@ -63,7 +145,7 @@ square location isAlive =
         , y (toString <| Matrix.row location)
         , width "1"
         , height "1"
-        , strokeWidth "0.05"
+        , strokeWidth "0.01"
         , stroke "black"
         , fill <|
             if isAlive then
@@ -71,4 +153,4 @@ square location isAlive =
             else
                 "white"
         ]
-        []
+        [ text (toString location) ]
